8ec38a35effd4f62e91985275055b63fd0494546
Gabriele <gabriele.guetta@outlook.com>
2025-05-27 21:26:03 +0300

    tournament

--- tournament.py
+++ tournament.py
@@ -2,16 +2,18 @@ from anl2025 import anl2025_tournament, make_multideal_scenario
 import pathlib
 from anl2025.negotiator import Boulware2025, Random2025, Linear2025
 from anl2025.scenario import MultidealScenario
-from myagent import job_dinner_agent  
-
+from myagent.job_dinner_agent import ImprovedUnifiedNegotiator
+from myagent.itay_agent import ItayNegotiator
+from myagent.dinners_agent import DinnersNegotiator
+from myagent.job_henter_agent import JobHunterNegotiator
 
 generated_scenario = make_multideal_scenario(nedges=3)
-path = pathlib.Path("official_test_scenarios/TargetQuantity_example")
+path = pathlib.Path("new_test_scenarios/supply_chain")
 scenario = MultidealScenario.from_folder(path)
 results = anl2025_tournament(
     scenarios=[scenario],
     n_jobs=-1,
-    competitors=(Boulware2025, Linear2025, job_dinner_agent),
+    competitors=(Boulware2025, Linear2025, JobHunterNegotiator, DinnersNegotiator, ImprovedUnifiedNegotiator, ItayNegotiator),
     verbose=True,
      no_double_scores=False,
 )
