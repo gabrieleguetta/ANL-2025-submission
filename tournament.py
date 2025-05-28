bee08e2ab55e386a686b635fab19e56ae20b7cdf
Gabriele <gabriele.guetta@outlook.com>
2025-05-28 17:10:21 +0300

    new tests

--- tournament.py
+++ tournament.py
@@ -8,12 +8,12 @@ from myagent.dinners_agent import DinnersNegotiator
 from myagent.job_henter_agent import JobHunterNegotiator
 
 generated_scenario = make_multideal_scenario(nedges=3)
-path = pathlib.Path("new_test_scenarios/supply_chain")
+path = pathlib.Path("new_test_scenarios/service_provider")
 scenario = MultidealScenario.from_folder(path)
 results = anl2025_tournament(
     scenarios=[scenario],
     n_jobs=-1,
-    competitors=(Boulware2025, Linear2025, JobHunterNegotiator, DinnersNegotiator, ImprovedUnifiedNegotiator, ItayNegotiator),
+    competitors=(Boulware2025, Linear2025, Random2025),
     verbose=True,
      no_double_scores=False,
 )
