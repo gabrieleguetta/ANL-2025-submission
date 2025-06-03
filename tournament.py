4269095938035cad4d26040913c822c894a61068
itayely <itay.elyashiv@gmail.com>
2025-06-03 17:13:43 +0300

    hyperparameters 2

--- tournament.py
+++ tournament.py
@@ -7,13 +7,23 @@ from myagent.itay_agent import ItayNegotiator
 from myagent.dinners_agent import DinnersNegotiator
 from myagent.job_henter_agent import JobHunterNegotiator
 
+
 generated_scenario = make_multideal_scenario(nedges=3)
-path = pathlib.Path("new_test_scenarios/service_provider")
-scenario = MultidealScenario.from_folder(path)
+# TargetQuantity_example
+# TargetQuantity_example
+# job_hunt_target
+path1 = pathlib.Path("official_test_scenarios/TargetQuantity_example")
+path2 = pathlib.Path("official_test_scenarios/dinners")
+path3 = pathlib.Path("official_test_scenarios/job_hunt_target")
+
+scenario1 = MultidealScenario.from_folder(path1)
+scenario2 = MultidealScenario.from_folder(path2)
+scenario3 = MultidealScenario.from_folder(path3)
 results = anl2025_tournament(
-    scenarios=[scenario],
+    scenarios=[scenario1],
+    n_repetitions=10,
     n_jobs=-1,
-    competitors=(Boulware2025, Linear2025, Random2025),
+    competitors=(ItayNegotiator, Boulware2025),
     verbose=True,
      no_double_scores=False,
 )
